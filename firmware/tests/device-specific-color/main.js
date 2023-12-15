802c90ad7e0113c4f1cf37a1c19594fa453d4113
kurasawa <kurasawa@rt-net.jp>
2023-12-15 16:00:57 +0900

    macアドレスからデバイス固有の色をディスプレイに表示する機能を記述した

---
+++ firmware/tests/device-specific-color/main.js
@@ -0,0 +1,10 @@
+import { getDeviceSpecificColor } from "color"
+import Poco from 'commodetto/Poco'
+
+const poco = new Poco(globalThis.screen)
+
+const [r, g, b] = getDeviceSpecificColor()
+const color = poco.makeColor(r, g, b)
+poco.begin(0, 0, poco.width, poco.height)
+poco.fillRectangle(color, 0, 0, poco.width, poco.height)
+poco.end()
