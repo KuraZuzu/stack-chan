a860ccf7c532a71b5c8349ec0a4094ec93f80197
kurasawa <kurasawa@rt-net.jp>
2023-12-20 17:03:56 +0900

    LCD色変更プログラムの取り込みのためにホスト側でモジュール"color"のビルドとcolorのmodの作成 [補足]color/mod.jsのcolorモジュールのインポートで失敗する

---
+++ firmware/mods/color/mod.js
@@ -0,0 +1,15 @@
+import { getDeviceSpecificColor } from "color"
+
+export function onRobotCreated(robot) {
+    /**
+     * Change color
+     */
+    const [r, g, b] = getDeviceSpecificColor()
+    let flag = false
+    robot.button.c.onChanged = function () {
+        if (this.read()) {
+            robot.setColor('secondary', r, g, b)
+            flag = !flag
+        }
+    }
+}
