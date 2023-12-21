6b8eec8db3f4420a9a992cb594273a5ec60e7acf
kurasawa <kurasawa@rt-net.jp>
2023-12-21 12:01:21 +0900

    ややこしくなるので、デフォルトで色が代わった状態にした（Cボタンで色変えするのを消した）

--- firmware/mods/color/mod.js
+++ firmware/mods/color/mod.js
@@ -5,11 +5,5 @@ export function onRobotCreated(robot) {
      * Change color
      */
     const [r, g, b] = getDeviceSpecificColor()
-    let flag = false
-    robot.button.c.onChanged = function () {
-        if (this.read()) {
-            robot.setColor('secondary', r, g, b)
-            flag = !flag
-        }
-    }
+    robot.setColor('secondary', r, g, b)
 }
